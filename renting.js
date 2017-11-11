328c71346ae5ff769aa3a870da7018119869cdb2
boribet <boribetlen@gmail.com>
2017-11-11 11:53:49 +0000

    ALERT of searched category solved

--- renting.js
+++ renting.js
@@ -1,14 +1,15 @@
 
 console.log(document.getElementById("submitFilmQuery"));
 document.getElementById("submitFilmQuery").addEventListener("click",informSelected);
-document.getElementById("movie_categories").addEventListener("change",function() {console.log(document.getElementById("movie_categories").selectedIndex)});
+//document.getElementById("movie_categories").addEventListener("change",function() {alert(document.getElementById("movie_categories").selectedIndex)});
 
 function informSelected() {
-  var x = document.getElementById("movie_categories").selectedIndex;
-  var y = document.getElementById("txtFilmQuery");
+var x = document.getElementById("movie_categories").selectedIndex;
+var y = document.getElementById("movie_categories").options;
+var z = document.getElementById("txtFilmQuery");
   if (x != 0) {
-    alert(x.value);
+    alert("You searched for: " + y[x].text);
   } else {
-    alert(y.value);
-  }
+    alert("You searched for: " + z.value); }
+
 }
