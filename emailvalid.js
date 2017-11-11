bfb952b9b3178f0140f27106f2b123a3333e9708
boribet <boribetlen@gmail.com>
2017-11-11 13:10:10 +0000

    Email validation length check added

--- emailvalid.js
+++ emailvalid.js
@@ -1,12 +1,13 @@
 console.log(document.getElementById("submit"));
 document.getElementById("submit").addEventListener("click", checkEmail);
 
+
 function checkEmail() {
 
     var email = document.getElementById('txtEmail');
     var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    if (!filter.test(email.value)) {
+    if (!filter.test(email.value) || email.value.length>50) {
       alert('Please provide a valid email address');
       email.focus;
       return false;
